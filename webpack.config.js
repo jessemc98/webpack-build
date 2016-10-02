80c0616e8067c72586e2c0e601b13b0c1fac504f
Jesse <Jesse-Mac@Jesses-MBP.lan>
2016-10-02 19:37:44 +0200

    added babel/es2015 support

--- webpack.config.js
+++ webpack.config.js
@@ -4,4 +4,11 @@
       path: './bin',
       filename: 'bundle.js'
     }
+    module: {
+      loaders: [{
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }]
+    }
  };
