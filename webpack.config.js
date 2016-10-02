dc7ad8c868f2ea411f37375ea745a75adb061998
Jesse <Jesse-Mac@Jesses-MBP.lan>
2016-10-02 20:03:00 +0200

    added uglifyJS plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,26 @@
- module.exports = {
-    entry: './src/main.js',
-    output: {
-      path: './bin',
-      filename: 'bundle.js'
-    },
-    module: {
-      loaders: [{
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader'
-      }]
-    }
- };
+const webpack = require('webpack')
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    path: './bin',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader'
+    }]
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false,
+      },
+      output: {
+        comments: false,
+      }
+    })
+  ]
+}
